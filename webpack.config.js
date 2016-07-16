9d4ad7a562754365ceaba4547d1f31c08ba0e7c1
Dong Chen <chen_dong85@foxmail.com>
2016-07-16 11:13:02 +0800

    set the config to sperate dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,64 @@
 var webpack = require('webpack');
-module.exports = {
-  devtool: 'source-map',
-  entry: {
-    app:[
-     'webpack-dev-server/client?http://localhost:8080',
-     'webpack/hot/only-dev-server',
-     './src/application.js'
+var active;
+var config = {
+  dev:{
+    devtool: 'source-map',
+    entry: {
+      app:[
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/application.js'
+      ]
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: '[name].bundle.js'
+    },
+    module:{
+      loaders:[
+        {test:/\.css$/,loader:'style!css'},
+        {test:/\.html$/,loader:'ng-cache-loader'}
+      ]
+    },
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin() // Wire in the hot loading plugin
     ]
   },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: '[name].bundle.js'
-  },
-  module:{
-    loaders:[
-      {test:/\.css$/,loader:'style!css'},
-      {test:/\.html$/,loader:'ng-cache-loader'}
+
+  prod:{
+    entry: {
+      app:[
+        './src/application.js'
+      ]
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: '[name].bundle.js'
+    },
+    module:{
+      loaders:[
+        {test:/\.css$/,loader:'style!css'},
+        {test:/\.html$/,loader:'ng-cache-loader'}
+      ]
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({compress: {warnings: false, drop_console: true},mangle: false,sourceMap: false}),
     ]
-  },
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(), // Wire in the hot loading plugin
-    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false, drop_console: true},mangle: false,sourceMap: false}),
-  ]
+  }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  active = config.prod
+}else if(process.env.NODE_ENV === 'development'){
+  active = config.dev
+}else {
+  active = config.dev
+}
+
+module.exports = active;
