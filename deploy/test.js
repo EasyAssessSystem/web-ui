f2e8074e967e1f1e21ee4b02299c710e5ac33009
Li Cheng <chengli@thoughtworks.com>
2016-07-25 17:01:00 +0800

    remove usless env config settings

--- deploy/test.js
+++ deploy/test.js
@@ -1,22 +1,4 @@
 var environments = {
-    'dev': {
-        pdm: function(domain) {
-            return 'http://localhost:8180/' + (domain ? domain : EasyAssess.session.domain) + '/data/'
-        },
-
-        assess: function(domain) {
-            return 'http://localhost:9190/' + (domain ? domain : EasyAssess.session.domain) + '/assess/'
-        }
-    },
-    'prod':{
-        pdm: function(domain) {
-            return 'http://localhost:8180/' + (domain ? domain : EasyAssess.session.domain) + '/data/'
-        },
-
-        assess: function(domain) {
-            return 'http://localhost:9190/' + (domain ? domain : EasyAssess.session.domain) + '/assess/'
-        }
-    },
     'test':{
         pdm: function(domain) {
             return 'http://localhost:8180/' + (domain ? domain : EasyAssess.session.domain) + '/data/'
