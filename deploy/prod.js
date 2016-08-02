203cf3649bb2c326d062d3395dc574e4a4fe749d
Li Cheng <chengli@thoughtworks.com>
2016-08-02 11:24:03 +0800

    fix cookie domain issue

--- deploy/prod.js
+++ deploy/prod.js
@@ -1,6 +1,6 @@
 var prod = {
-    pdm:'http://103.227.51.161:8180/',
-    assess:'http://103.227.51.161:9190/'
+    pdm:'http://www.thethirdqc.com:8180/',
+    assess:'http://www.thethirdqc.com:9190/'
 };
 
 window._activeEnv = prod;
