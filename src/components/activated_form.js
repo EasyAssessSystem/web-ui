2bce2a700a0091b5b5856cf03bcd9afd326dea10
Li Cheng <chengli@thoughtworks.com>
2017-10-16 06:39:33 +0800

    View submitted form

--- src/components/activated_form.js
+++ src/components/activated_form.js
@@ -1,5 +1,5 @@
 var EasyAssess = require('../easyassess.application');
-EasyAssess.app.ActivatedFormController = function($scope,$state) {
+EasyAssess.app.ActivatedFormController = function($scope,$state,esRequestService) {
     this.initialize.apply(this, arguments);
 };
 
@@ -21,13 +21,34 @@ EasyAssess.app.ActivatedFormController .prototype = EasyAssess.extend({
         };
 
         $scope.$on('submitted',function(){
+            $scope.back();
+        });
+
+        $scope.back = function () {
             $scope.activeModel = null;
-        })
+            $scope.template = null;
+        }
     },
 
     _select: function(model) {
+        this.$scope.template = null;
         if (model.status == "A") {
             this.$scope.activeModel = model;
+        } else if (model.status == "C") {
+            var url = EasyAssess.activeEnv['assess']() + 'template/' + model.securedAssessment.templateGuid;
+            this.esRequestService.esGet(url).then((function(data){
+                for (var code in model.securedAssessment.specimenCodes) {
+                    data.data.groups.forEach(function (group) {
+                        group.specimens.forEach(function (specimen) {
+                            if (specimen.number === code) {
+                                specimen.isPlainNumber = true;
+                            }
+                        });
+                    })
+                }
+                this.$scope.activeModel = model;
+                this.$scope.template = data.data;
+            }).bind(this));
         }
     },
 }, EasyAssess.app.MaintenanceController.prototype);
